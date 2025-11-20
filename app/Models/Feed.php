425bd955be14e0617bd8eb8b7ed7458b0bf44520
Aliu <aliuwahab@gmail.com>
2025-11-20 10:40:43 +0100

    generate models for feeds and feeditems for darta access

---
+++ app/Models/Feed.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Feed extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'user_id',
+        'name',
+        'url',
+        'last_processed_at',
+    ];
+
+    /**
+     * Get the user that owns the feed.
+     */
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    /**
+     * Get the items for the feed.
+     */
+    public function items(): HasMany
+    {
+        return $this->hasMany(FeedItem::class);
+    }
+}
---
+++ app/Models/FeedItem.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class FeedItem extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'feed_id',
+        'title',
+        'link',
+        'entry_id',
+        'published_at',
+    ];
+
+    /**
+     * The attributes that should be cast.
+     *
+     * @var array<string, string>
+     */
+    protected $casts = [
+        'published_at' => 'datetime',
+    ];
+
+    /**
+     * Get the feed that owns the item.
+     */
+    public function feed(): BelongsTo
+    {
+        return $this->belongsTo(Feed::class);
+    }
+}
